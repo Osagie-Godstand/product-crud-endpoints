116ff60263d9da458106252246f66679e8ea08f0
Osagie-Godstand <51753700+Osagie-Godstand@users.noreply.github.com>
2023-09-03 21:59:28 +0100

    crud-product-endpoints

--- migrations/20230822120000_create_products_table.up.sql
+++ migrations/20230822120000_create_products_table.up.sql
@@ -1,3 +1,5 @@
+DO $$ BEGIN
+    IF NOT EXISTS (SELECT 1 FROM information_schema.tables WHERE table_name = 'products') THEN
 CREATE TABLE IF NOT EXISTS products (
     id SERIAL PRIMARY KEY,
     name TEXT,
@@ -5,3 +7,5 @@ CREATE TABLE IF NOT EXISTS products (
     price DOUBLE PRECISION,
     sku TEXT
 );
+ END IF;
+END $$;
